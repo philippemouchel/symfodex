089736476ba7d1d8fb26b9d7388e97c5c771142d
Philippe Mouchel <philippe@symetris.ca>
2020-04-08 11:29:08 -0400

    Provide a createPokemon controller to create Polemon entities, using relations methods.

--- app/src/Controller/PokemonController.php
+++ app/src/Controller/PokemonController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use App\Entity\Category;
+use App\Entity\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class PokemonController extends AbstractController
 {
@@ -15,5 +20,43 @@ class PokemonController extends AbstractController
         return $this->render('pokemon/index.html.twig', [
             'controller_name' => 'PokemonController',
         ]);
+    }
+
+    /**
+     * @Route("/create/pokemon", name="create_pokemon")
+     * @return Response
+     */
+    public function createPokemon()
+    {
+        // Instantiate entity repositories.
+        $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
+        $typeRepository = $this->getDoctrine()->getRepository(Type::class);
+
+        // Init new Pokemon object.
+        $pokemon = new Pokemon();
+        $pokemon->setNumber(9);
+        $pokemon->setName('Blastoise');
+        $pokemon->setDescription('Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.');
+        $pokemon->setHeight(1600); // millimeters.
+        $pokemon->setWeight(85500); // grams.
+
+        // Relates this pokemon to the category.
+        $category = $categoryRepository->findOneBy(['name' => 'Shellfish']);
+        $pokemon->setCategory($category);
+
+        // Relates this pokemon to the category.
+        $types = [
+            $typeRepository->findOneBy(['name' => 'Water']),
+//            $typeRepository->findOneBy(['name' => 'Flying']),
+        ];
+        foreach ($types as $type) {
+            $pokemon->addType($type);
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($pokemon);
+        $entityManager->flush();
+
+        return new Response('<html><body><p>Saved new pokemon <strong>' . $pokemon->getName() . ' (' . $pokemon->getNumber() . ')</strong></p></body></html>');
     }
 }
