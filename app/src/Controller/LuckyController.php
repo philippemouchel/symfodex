20426df2315535ced86ed597288ccace8e666c87
Philippe Mouchel <philippe@symetris.ca>
2020-04-07 21:57:27 -0400

    Implement basic route (annotation), controller and template (twig and extending).

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -4,22 +4,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController {
+class LuckyController extends AbstractController {
 
     /**
-     * @Route("/lucky/number")
+     * @Route(
+     *     "/lucky/number/{max}",
+     *     name="tuto_lucky_number",
+     *     requirements={"max"="\d+"},
+     *     methods={"GET"},
+     * )
      *
+     * @param int $max
      * @return Response
      * @throws \Exception
      */
-    public function number() {
-        $number = random_int(0, 100);
+    public function number(int $max = 100) {
+        $number = random_int(0, $max);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'max' => $max,
+            'number' => $number,
+        ]);
     }
 }
