8bcef19f409719e67a758e1771504fa989f568ba
Philippe MOUCHEL <philippe.mouchel@gmail.com>
2020-05-29 07:41:55 -0400

    3: test locale value access and use it to display FR or EN labels.

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -1,11 +1,10 @@
 <?php
 
-// src/Controller/LuckyController.php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController extends AbstractController
@@ -18,17 +17,20 @@ class LuckyController extends AbstractController
      *     methods={"GET"},
      * )
      *
+     * @param Request $request
      * @param int $max
      * @return Response
      * @throws \Exception
      */
-    public function number(int $max = 100)
+    public function number(Request $request, int $max = 100)
     {
         $number = random_int(0, $max);
+        $locale = $request->getLocale();
 
         return $this->render('lucky/number.html.twig', [
             'max' => $max,
             'number' => $number,
+            'locale' => $locale,
         ]);
     }
 }
