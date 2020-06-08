a88a16829d2a3ce7c9e01d891428fd2700dc487f
Philippe Mouchel <philippe@symetris.ca>
2020-06-08 19:49:04 -0400

    SFD-7: add dependency to KNP Paginator bundle.

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -19,4 +19,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
